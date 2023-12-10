1892bb0ae8840b2986b1cd633e2aff044b0fda38
Mattias Axelsson <mattiaax@axis.com>
2023-12-10 08:30:11 +0100

    9

---
+++ 2023/9b.lisp
@@ -0,0 +1,16 @@
+
+(defparameter lines '(
+(0 3 6 9 12 15)
+(1 3 6 10 15 21)
+(  10 13 16 21 30 45)))
+
+(load "9-data.lisp")
+
+(defun diff (a) (mapcar #'- (cdr a) a))
+
+(defun predict (a)
+  (if (every #'zerop a)
+      0
+      (- (car a) (predict (diff a)))))
+
+(defparameter sum (reduce #'+ (mapcar #'predict lines)))
