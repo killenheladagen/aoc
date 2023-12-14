240572dd94c36ffa6ad8fa9314a3afe339036e0a
Mattias Axelsson <mattiaax@axis.com>
2023-12-15 00:29:51 +0100

    12b expand

--- 2023/12b.cpp
+++ 2023/12b.cpp
@@ -4,8 +4,10 @@
 #include <map>
 #include <set>
 #include <string>
+#include <thread>
 #include <tuple>
 #include <vector>
+
 using namespace std;
 
 auto input = vector<tuple<string, vector<int>>> {
@@ -16,7 +18,7 @@ auto input = vector<tuple<string, vector<int>>> {
 #endif
 };
 
-enum foo { INVALID = 0, VALID = 10, MAYBE = 5 };
+enum foo { INVALID = 0, VALID = 1, MAYBE = 42 };
 
 foo test_validity(string const &pat, vector<int> const &m) {
     auto p = pat.c_str();
@@ -44,6 +46,7 @@ foo test_validity(string const &pat, vector<int> const &m) {
     return has_q ? MAYBE : VALID;
 }
 
+#if 0
 bool next_cand(string &pat) {
     bool carry = true;
     for (auto &c : pat) {
@@ -60,6 +63,7 @@ bool next_cand(string &pat) {
     }
     return !carry;
 }
+#endif
 #if 0
 bool prev_cand(string &pat) {
     for (int i = pat.length() - 1; i >= 0; i--) {
@@ -73,22 +77,38 @@ bool prev_cand(string &pat) {
 }
 #endif
 
-int64_t num_combos(string pat, vector<int> const &m, int x = 0,
-                   bool verbose = false) {
-    if (x == 1) {
-        auto patx = pat + "?" + pat;
-        vector<int> mx(m.size() * 2);
-        for (int i = 0; i < mx.size(); i++)
-            mx[i] = m[i % m.size()];
-        return num_combos(patx, mx, 0, verbose);
-    } else if (x == 4) {
-        auto a = num_combos(pat, m, 0, verbose);
-        auto b = num_combos(pat, m, 1, verbose);
-        auto c = a * (int(round(pow(b / a, 4))));
-        cerr << "\n f(" << a << ", " << b << ") => " << c;
-        return c;
+auto expand(tuple<string, vector<int>> const &tm, int n) {
+    auto pat0 = get<0>(tm);
+    auto m0 = get<1>(tm);
+    auto pat = pat0;
+    for (int i = 0; i < n; i++)
+        pat += "?" + pat0;
+    vector<int> m(m0.size() * (n + 1));
+    for (int i = 0; i < m.size(); i++)
+        m[i] = m0[i % m0.size()];
+    return make_tuple(pat, m);
+}
+
+int64_t num_combos(tuple<string, vector<int>> const &pm, int x = 0,
+                   bool verbose = false, bool force_calc = false) {
+    if (!force_calc && x == 4) {
+        verbose = false;
+        auto a = num_combos(pm, 0, verbose);
+        auto b = num_combos(pm, 1, verbose);
+        auto b_div_a = (double)b / a;
+        if (fabs(b_div_a - round(b_div_a)) < 0.001) {
+            auto c = a * (int64_t(round(pow(b_div_a, 4))));
+            cerr << "\n f(" << a << ", " << b << ") => " << c;
+            return c;
+        }
+        cerr << "  " << b << " / " << a << " = " << b_div_a << " ...";
+        return num_combos(pm, x, true, true);
+    } else if (x > 0) {
+        return num_combos(expand(pm, x), 0, verbose);
     }
 
+    auto pat = get<0>(pm);
+    auto const &m = get<1>(pm);
     if (verbose) {
         cerr << "\n" << pat << " [";
         for (auto e : m)
@@ -96,24 +116,36 @@ int64_t num_combos(string pat, vector<int> const &m, int x = 0,
         cerr << "]";
     }
 
-    auto num_q = count(pat.begin(), pat.end(), '?');
-    if (num_q == 0)
-        return (test_validity(pat, m) == VALID) ? 1 : 0;
+    auto v = test_validity(pat, m);
+    if (v != MAYBE) {
+        return v;
+    }
 
     auto p = &pat[0];
     while (*p != '?')
         p++;
     *p = '0';
-    auto r0 = num_combos(pat, m);
+    auto r0 = num_combos({pat, m});
     *p = '1';
-    auto r1 = num_combos(pat, m);
+    auto r1 = num_combos({pat, m});
     return r0 + r1;
 }
 
 int main() {
+    thread t([]() {
+        this_thread::sleep_for(10s);
+        abort();
+    });
+
     int64_t sum = 0;
-    for (auto const &e : input)
-        sum += num_combos(get<0>(e), get<1>(e), 4, true);
+    for (auto const &e : input) {
+        auto res = num_combos(e, 4, true);
+        cerr << ": " << res;
+        sum += res;
+        cerr << "  (sum=" << sum << ")";
+    }
     cerr << "\n\n" << sum << "\n";
+    if (sum <= 91464976098328)
+        cerr << "TOO LOW\n";
     return 0;
 };
