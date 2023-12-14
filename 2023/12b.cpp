cefbd94f291e75948653fc19f18a837a547fd9b4
Mattias Axelsson <mattiaax@axis.com>
2023-12-14 22:54:40 +0100

    wip 12b cpp

--- 2023/12b.cpp
+++ 2023/12b.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <map>
 #include <set>
@@ -7,9 +8,12 @@
 #include <vector>
 using namespace std;
 
-auto input = vector<tuple<string, vector<int>>>{
-//#include "12-input.cpp"
+auto input = vector<tuple<string, vector<int>>> {
+#if 1
+#include "12-input.cpp"
+#else
 #include "12-ex.cpp"
+#endif
 };
 
 enum foo { INVALID = 0, VALID = 10, MAYBE = 5 };
@@ -68,39 +72,51 @@ bool prev_cand(string &pat) {
     return false;
 }
 #endif
-int64_t num_combos(string const &pat0, vector<int> const &m0) {
+
+int64_t num_combos(string pat, vector<int> const &m, int x) {
     // cerr << "\n"
     //        << pat0 << " " << pat0 << " " << pat0 << " " << pat0 << " " <<
     //        pat0;
-    auto pat = pat0 + "?" + pat0 + "?" + pat0 + "?" + pat0 + "?" + pat0;
-    vector<int> m(m0.size() * 5);
-    for (int i = 0; i < m.size(); i++)
-        m[i] = m0[i % m0.size()];
 
-        // pat = pat0;
-        // m = m0;
+    if (x == 1) {
+        auto patx = pat + "?" + pat;
+        vector<int> mx(m.size() * 2);
+        for (int i = 0; i < mx.size(); i++)
+            mx[i] = m[i % m.size()];
+        return num_combos(patx, mx, 0);
+    } else if (x == 4) {
+        auto a = num_combos(pat, m, 0);
+        auto b = num_combos(pat, m, 1);
+        auto c = a * (int(round(pow(b / a, 4))));
+        cerr << "\n f(" << a << ", " << b << ") => " << c;
+        return c;
+    }
 
-#if 0
-    cerr << "\n" << pat << "   ";
+    cerr << "\n" << pat << " [";
     for (auto e : m)
         cerr << " " << e;
-    cerr << "   ";
-#endif
+    cerr << "]";
+
     int64_t res = 0;
+    string first_valid;
     while (next_cand(pat)) {
         auto s = test_validity(pat, m);
         // cerr << "\n   " << pat << " => " << s;
-        if (s == VALID)
+        if (s == VALID) {
+            if (first_valid.empty())
+                first_valid = pat;
             res++;
+        }
     }
-    cerr << "\n" << pat0 << "  " << res;
+    cerr << " => " << res;
+    cerr << " (" << first_valid << ")";
     return res;
 }
 
 int main() {
     int64_t sum = 0;
     for (auto const &e : input)
-        sum += num_combos(get<0>(e), get<1>(e));
+        sum += num_combos(get<0>(e), get<1>(e), 4);
     cerr << "\n\n" << sum << "\n";
     return 0;
 };
