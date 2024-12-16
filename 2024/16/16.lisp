7a1dba98f2ca3b96188fe02006f883a2bd935c5c
Mattias Axelsson <mattiaax@axis.com>
2024-12-17 00:01:43 +0100

    16a wip min-cost check

--- 2024/16/16.lisp
+++ 2024/16/16.lisp
@@ -1,18 +1,5 @@
 (load "board")
 
-(defun paths-to (end path b)
-  (if (= end (car path))
-      (list path)
-      (flet ((follow-path (pos)
-               (paths-to end (cons pos path) b))
-             (wall-or-visited (pos)
-               (or (eq (at pos b) #\#) (find pos path)))
-             (step-in-dir (dir) (+ (car path) dir)))
-        (mapcan #'follow-path
-                (remove-if #'wall-or-visited
-                           (mapcar #'step-in-dir *north-east-south-west*))))))
-(compile 'paths-to)
-
 (defun cost (path)
   (labels ((f (p dir)
              (if (< (length p) 2)
@@ -20,14 +7,41 @@
                  (+ (if (= (+ (car p) dir) (cadr p)) 1 1001)
                     (f (cdr p) (- (cadr p) (car p)))))))
     (f (reverse path) 1)))
-(compile 'cost)
 
-(defun lowest-reindeer-score (file)
-  (let* ((b (read-board-file file))
-         (start (car (find-eq #\S b)))
-         (end (car (find-eq #\E b))))
-    (reduce #'min (mapcar #'cost (paths-to end (list start) b)))))
-(compile 'lowest-reindeer-score)
+(let ((min-cost))
+  (defun paths-to (end cost path b)
+    (flet ((d0 (list) (- (cadr list) (car list))))
+      (incf cost (if (cdr path)
+                     (if (= (d0 path)
+                            (if (cddr path)
+                                (d0 (cdr path))
+                                1)) ;; Start in eastern dir
+                         1 1001)
+                     0)))
+    (when (or (not min-cost) (< cost min-cost))
+      (if (= end (car path))
+          (progn
+            (print path)
+            (print (cost path))
+            (setf min-cost (min cost (or min-cost cost)))
+            (list path))
+          (flet ((follow-path (pos)
+                   (paths-to end cost (cons pos path) b))
+                 (wall-or-visited (pos)
+                   (or (eq (at pos b) #\#) (find pos path)))
+                 (step-in-dir (dir) (+ (car path) dir)))
+            (mapcan #'follow-path
+                    (remove-if #'wall-or-visited
+                               (mapcar #'step-in-dir *north-east-south-west*)))))))
+
+  (defun lowest-reindeer-score (file)
+    (setf min-cost nil)
+    (let* ((b (read-board-file file))
+           (start (car (find-eq #\S b)))
+           (end (car (find-eq #\E b))))
+      ;;(reduce #'min (mapcar #'cost (paths-to end (list start) b)))))
+      (paths-to end 0 (list start) b))
+    min-cost))
 
 (assert (= (lowest-reindeer-score "test.txt") 7036))
 (assert (= (lowest-reindeer-score "test2.txt")  11048))
