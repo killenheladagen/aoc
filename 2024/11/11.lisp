6a6f8ac0c1a33a8d0916b1ed11f2a145d98b5464
Mattias Axelsson <mattias@barbanet.com>
2024-12-31 12:41:48 +0100

    11b

--- 2024/11/11.lisp
+++ 2024/11/11.lisp
@@ -1,4 +1,5 @@
 (require 'uiop)
+(load "hash-table")
 
 (defun transform-stone (x)
   (let ((xs (format nil "~a" x)))
@@ -8,38 +9,34 @@
              (mapcar #'parse-integer (list (subseq xs 0 half) (subseq xs half)))))
           (t (list (* 2024 x))))))
 
-(let ((cache make-hash-table))
-  (defun stone-after (blinks x)
-    (let ((cache-ref (gethash x cache)))
-      (unless cache-ref
-        (setf cache-ref (make-array 76 :initial-element nil))
-        (setf (gethash x cache) cache-ref))
-      (if (< blinks (length cache-ref))
-          (nth blinks cache-ref)
-          (progn
-            (let ((blink-list
-                    (mapcar (lambda (n)
-                              (
-                               (setf cache-ref blink-list)
+(compile 'transform-stone)
 
+(defun stones-after (blinks stone-list)
+  (if (zerop blinks)
+      stone-list
+      (stones-after (1- blinks) (mapcan #'transform-stone stone-list))))
 
-                               (defun stones-after (blinks stone-list)
-                                 (format t "~a ~a~%" blinks (length stone-list))
-                                 (flet ((f (x)
-                                          (let ((xs (format nil "~a" x)))
-                                            (cond ((zerop x) (list 1))
-                                                  ((evenp (length xs))
-                                                   (let ((half (/ (length xs) 2)))
-                                                     (mapcar #'parse-integer (list (subseq xs 0 half) (subseq xs half)))))
-                                                  (t (list (* 2024 x)))))))
-                                   (if (zerop blinks)
-                                       stone-list
-                                       (stones-after (1- blinks) (mapcan #'f stone-list)))))
+(compile 'stones-after)
 
-                               (compile 'stones-after)
+(defun num-stones-after (blinks stone-list)
+  (let ((ht (make-hash-table)))
+    (mapc (lambda (stone)
+            (setf (gethash stone ht) 1))
+          stone-list)
+    (dotimes (i blinks)
+      (let ((prev-stones (copy-list (hash-keys-and-values ht))))
+        (setf ht (make-hash-table))
+        (mapc (lambda (stone-and-count)
+                (let ((new-stones (transform-stone (car stone-and-count)))
+                      (count (cdr stone-and-count)))
+                  (mapc (lambda (stone)
+                          (let ((old-count (gethash stone ht 0)))
+                            (setf (gethash stone ht) (+ old-count count))))
+                        new-stones)))
+              prev-stones)))
+    (reduce #'+ (hash-values ht))))
 
-                               (assert (equal (stones-after 6 '(125 17))
-                                              '(2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2)))
-                               (assert (= (length (stones-after 25 '(125 17))) 55312))
-                               (print (length (stones-after 25 '(77 515 6779622 6 91370 959685 0 9861))))
-                               (print (length (stones-after 75 '(77 515 6779622 6 91370 959685 0 9861))))
+(assert (= (num-stones-after 6 '(125 17)) 22))
+(assert (= (num-stones-after 25 '(125 17)) 55312))
+(assert (= (print (num-stones-after 25 '(77 515 6779622 6 91370 959685 0 9861))) 187738))
+(print (num-stones-after 75 '(77 515 6779622 6 91370 959685 0 9861)))
---
+++ 2024/11/hash-table.lisp
@@ -0,0 +1,8 @@
+(defun hash-keys (hash-table)
+  (loop for key being the hash-keys of hash-table collect key))
+
+(defun hash-keys-and-values (hash-table)
+  (mapcar (lambda (key) (cons key (gethash key hash-table))) (hash-keys hash-table)))
+
+(defun hash-values (hash-table)
+  (mapcar #'cdr (hash-keys-and-values hash-table)))
