1ea2aac48037afa049fe259f39b498fd799a6c9c
Mattias Axelsson <mattiaax@axis.com>
2025-12-05 09:48:51 +0100

    5b
    Change-Id: I1f3e269231675492e56bafda4ed6a33471c4e807

--- 2025/5/5.lisp
+++ 2025/5/5.lisp
@@ -27,8 +27,41 @@
              (remove-if-not (lambda (x) (in-range id x)) ranges)))
       (length (remove-if-not #'fresh-p ids)))))
 
-;;(defun count-unique-elements-in-ranges (file)
-;;  (let ((ranges (read-ingredients-file file)))
+(defun range-size (x)
+  (1+ (- (cdr x) (car x))))
 
-(aoc (lambda (file) (count-fresh-ingredients file)) 3)
-;;(lambda (file) (count-unique-elements-in-ranges file)) 14)
+(defun range-eq (a b)
+  (and (= (car a) (car b))
+       (= (cdr a) (cdr b))))
+
+(defun range-combine (a b)
+  (when (or (in-range (car a) b)
+            (in-range (cdr a) b)
+            (in-range (car b) a)
+            (in-range (cdr b) a))
+    (cons (min (car a) (car b))
+          (max (cdr a) (cdr b)))))
+
+(defun merge-overlapping-ranges (ranges &optional disjunct-ranges)
+  (if ranges
+      (let* ((first-range (car ranges))
+             (new-ranges (remove-if #'null
+                                    (mapcar
+                                     (lambda (x)
+                                       (let ((new-first (range-combine first-range x)))
+                                         (if new-first
+                                             (progn (setf first-range new-first)
+                                                    nil)
+                                             x)))
+                                     (cdr ranges)))))
+        (if (range-eq first-range (car ranges))
+            (merge-overlapping-ranges new-ranges (cons first-range disjunct-ranges))
+            (merge-overlapping-ranges (cons first-range new-ranges) disjunct-ranges)))
+      disjunct-ranges))
+
+(defun count-unique-elements-in-ranges (file)
+  (let ((ranges (read-ingredients-file file)))
+    (reduce #'+ (mapcar #'range-size (merge-overlapping-ranges ranges)))))
+
+(aoc (lambda (file) (count-fresh-ingredients file)) 3
+     (lambda (file) (count-unique-elements-in-ranges file)) 14)
